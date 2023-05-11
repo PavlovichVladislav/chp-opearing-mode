60479b6a66ba7c41bdf10e86a3be08e9fa805e19
PavlovichVladislav <jeffry2814@gmail.com>
2023-05-11 20:01:30 +0700

    calculated the OPRT for the boiler shop

--- src/pages/FuelConsumption.tsx
+++ src/pages/FuelConsumption.tsx
@@ -18,7 +18,7 @@ const calcHeatLoss = (payload: number[], efficiency: number[]) => {
    const result = [];
 
    for (let i = 0; i < payload.length; i++) {
-      result.push(payload[i] * (100 - efficiency[i]) / efficiency[i]);
+      result.push((payload[i] * (100 - efficiency[i])) / efficiency[i]);
    }
 
    return result;
@@ -65,13 +65,41 @@ const calcBoilerFuelConsumption = (relativeHeatLosses: number[], payload: number
    const result = [];
 
    for (let i = 0; i < relativeHeatLosses.length; i++) {
-    //   const value = 1 + relativeHeatLosses[i] / (payload[i + 1] - payload[i]);
+      //   const value = 1 + relativeHeatLosses[i] / (payload[i + 1] - payload[i]);
       const value = 1 + relativeHeatLosses[i];
 
       result.push(value * 0.1429);
    }
 
    return result;
+};
+
+// ОПРТ для котельного цеха
+// RIFC - опрт котла - массив опрт при разных нагрузках
+const calcBoilersFuelConsumption = (
+   boilersComposition: {
+      name: string;
+      count: number;
+      RIFC: { b: number; Q: number }[];
+   }[]
+) => {
+   // опрт цеха - результат функции
+   const shopRIFC = new Map<number, number>();
+
+   // перебираем пары ОПРТ для каждого котла
+   // новое значение - это старое значение для этого b(опрт при нагрузке)
+   // + новое значение - это очередной Q в переборе
+   // Резюмирую: взяли бойлер, взяли его RIFC перебрали, просуммировали для каждого b, соответствующие ему Q,
+   // если у нас 3 одинаковых болйера, то мы просто Q умножаем на boiler.count
+   // взяли второй бойлер, и такая же логика, b могут пересекаться, следовательно при одном b могут быть Q = Q1 * count1 + Q2 * count 2
+   boilersComposition.forEach((boiler) => {
+      boiler.RIFC.forEach(({ b, Q }) => {
+         const newValue = Q * boiler.count + (shopRIFC.get(b) || 0);
+         shopRIFC.set(b, newValue);
+      });
+   });
+
+   return shopRIFC;
 };
 
 const FuelConsumption = () => {
@@ -84,8 +112,32 @@ const FuelConsumption = () => {
    const relativeIncrsHeatLosses = calcRelativeHeatLosses(absoluteIncrsHeatLosses, payload);
    const boilerFuelConsumption = calcBoilerFuelConsumption(relativeIncrsHeatLosses, payload);
 
+   const shopRIFC = calcBoilersFuelConsumption([
+      {
+         name: "TP-170",
+         count: 3,
+         RIFC: [
+            { b: 0.152, Q: 76.25 },
+            { b: 0.154, Q: 78.75 },
+            { b: 0.157, Q: 82.5 },
+            { b: 0.158, Q: 87.2 },
+         ],
+      },
+      {
+         name: "TP-80",
+         count: 4,
+         RIFC: [
+            { b: 0.154, Q: 178.8 },
+            { b: 0.157, Q: 185 },
+            { b: 0.15, Q: 192.65 },
+            { b: 0.159, Q: 197.65 },
+         ],
+      },
+   ]);
+
    console.log(boilerFuelConsumption);
    console.log(averageLoadsValue);
+   console.log(shopRIFC);
 
    return <div></div>;
 };
